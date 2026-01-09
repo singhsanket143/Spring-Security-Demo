91e4a3d73529139f548a718fa9c0ce4d2b82adae
singhsanket143 <singhsanket143@gmail.com>
2026-01-09 16:02:03 +0000

    Added jwt auth filter logic

--- src/main/java/com/example/SpringSecurityDemo/configs/security/JwtAuthFilter.java
+++ src/main/java/com/example/SpringSecurityDemo/configs/security/JwtAuthFilter.java
@@ -2,8 +2,14 @@ package com.example.SpringSecurityDemo.configs.security;
 
 import java.io.IOException;
 
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
+
+import com.example.SpringSecurityDemo.services.UserService;
 
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
@@ -16,6 +22,7 @@ import lombok.RequiredArgsConstructor;
 public class JwtAuthFilter extends OncePerRequestFilter{
 
     private final JwtUtil jwtUtil;
+    private final UserService userService;
     
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
@@ -41,7 +48,32 @@ public class JwtAuthFilter extends OncePerRequestFilter{
             }
         }
 
-        
+        if (email != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+            try {
+                UserDetails userDetails = userService.loadUserByUsername(email);
+
+                if(jwtUtil.validateToken(jwt, userDetails)) {
+
+                    UsernamePasswordAuthenticationToken authenticationToken = new 
+                        UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+
+                    authenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                    // fill the SecurityContext with user details
+
+                    SecurityContextHolder.getContext().setAuthentication(authenticationToken);
+                } else {
+                    response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+                    response.getWriter().write("Invalid JWT token");
+                    return;
+                }
+            } catch (Exception e) {
+                response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+                response.getWriter().write("Invalid JWT token");
+                return;
+            }
+        }
+
+        filterChain.doFilter(request, response);
     }
 }
 
